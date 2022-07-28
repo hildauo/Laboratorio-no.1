39586128da9621e1279e0c436661c73855d30019
Hilda Orozco <110194209+hildauo@users.noreply.github.com>
2022-07-28 14:07:48 -0600

    Add files via upload

---
+++ Laboratorio no.1.py
@@ -0,0 +1,52 @@
+#Laboratorio no.1
+class EstadosMateria:
+    pass
+    def solido():
+        print("Sólido")
+        print("Tipo de materia: Materia Fija.")
+        print("Atracción entre partículas: Alta.")
+        print("Movilidad de partículas: Baja.")
+        print("Volumen: Con volumen.")
+        print("Forma: Definida.")
+        print("Ejemplo: Las piedras")
+        print("\n")
+
+    def liquido():
+        print("Liquído")
+        print("Tipo de materia: Fluidos con viscosidad.")
+        print("Atracción entre partículas: Intermedia.")
+        print("Movilidad de partículas: Intermedia.")
+        print("Volumen: Con volumen.")
+        print("Forma: Indefinida.")
+        print("Ejemplo: El agua")
+        print("\n")
+
+    def gaseoso():
+        print("Gaseoso")
+        print("Tipo de materia: Gases.")
+        print("Atracción entre partículas: Baja.")
+        print("Movilidad de partículas: Alta.")
+        print("Volumen: Sin volumen.")
+        print("Forma: Indefinida.")
+        print("Ejemplo: El vapor del agua")
+        print("\n")
+
+    def plasmatico():
+        print("Plasmático")
+        print("Tipo de materia: Gases calientes (Con carga eléctrica).")
+        print("Atracción entre partículas: Baja.")
+        print("Movilidad de partículas: Alta.")
+        print("Volumen: Sin volumen.")
+        print("Forma: Indefinida.")
+        print("Ejemplo: El TV de plasma")
+        print("\n")
+
+Solido = EstadosMateria
+Liquido = EstadosMateria
+Gaseoso = EstadosMateria
+Plasmatico = EstadosMateria
+
+EstadosMateria.solido()
+EstadosMateria.liquido()
+EstadosMateria.gaseoso()
+EstadosMateria.plasmatico()
